04f537851df2f2297ee3385abb0c16a2fa4ef67e
FUJIWARA Shunichiro <fujiwara.shunichiro@gmail.com>
2014-07-01 11:45:56 +0900

    patch command is required to build ruby

--- final/chef/setup-chef.sh
+++ final/chef/setup-chef.sh
@@ -2,7 +2,7 @@
 
 set -e
 cd /tmp
-yum -y install make gcc libyaml ruby ruby-libs ruby-rdoc ruby-irb ruby-devel
+yum -y install make gcc libyaml ruby ruby-libs ruby-rdoc ruby-irb ruby-devel patch
 curl -O http://production.cf.rubygems.org/rubygems/rubygems-1.8.25.tgz
 tar zxf rubygems-1.8.25.tgz
 cd rubygems-1.8.25
